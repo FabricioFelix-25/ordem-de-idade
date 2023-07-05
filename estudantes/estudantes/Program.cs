5848aac52e643f90c08bdaafb47815466a8713df
Fabricio felix <126669160+FabricioFelix-25@users.noreply.github.com>
2023-07-05 16:12:11 -0300

    Add files via upload

---
+++ estudantes/estudantes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace estudantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] estudantes = new string[5];
+            int[] idade = new int[5];
+            String[] lista = new string[5];  
+
+            Console.WriteLine("digite nome de 5 estudantes e suas idades");
+
+
+            for (int i = 0; i < estudantes.Length; i++)
+            {
+                Console.WriteLine("nome do estudante:");
+                estudantes[i] = Console.ReadLine();
+
+                Console.WriteLine("idade");
+                idade[i] = Convert.ToInt16(Console.ReadLine());
+           
+                 if (idade[i] <5 )
+                {
+                    estudantes[i] = "0" + idade[i] + " " + estudantes[i];
+
+                }
+                else
+                {
+                    lista[i] = idade[i] + " " + estudantes[i];               
+                }   
+
+                 
+            }
+
+            Array.Sort(lista);
+            foreach (String i in lista)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("o estudante mais velho é:" + lista[4]);
+            Console.WriteLine("o estudante mais novo é:" + lista[0]);
+        }
+    }
+
+} 
